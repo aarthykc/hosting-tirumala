06d37088df6c6db6b5972870ad03b38d6f40dc23
Aarthy Chandrasekhar <aarthychandrasekhar@Aarthys-MacBook-Air.local>
2015-08-06 11:33:31 +0530

    Changes made to geocode to include wiki api endpoint

--- js/geocoding.js
+++ js/geocoding.js
@@ -1,19 +1,29 @@
 
 function geocode_search()
-{geocoder.on('found', function(data)
-    {
-      console.log(data);
-    });
 
- geocoder.on('select', function(data) {
+{ var url = 'https://en.wikipedia.org/w/api.php?action=query&prop=info&format=json&inprop=url&titles=';
+geocoder.on('found', function(data)
+{
+	console.log(data);
+});
 
-  console.log(data);
-  map.removeLayer(marker);
+geocoder.on('select', function(data) {
 
-  var lon = data['feature']['center'][0];
-  var lat = data['feature']['center'][1];
-  var place = data['feature']['geometry']['place_name'];
-  
-  marker = new L.Marker([lat, lon]).addTo(map);
+	console.log(data);
+	map.removeLayer(marker);
+
+	var lon = data['feature']['center'][0];
+	var lat = data['feature']['center'][1];
+	var place = data['feature']['place_name'];
+	marker = new L.Marker([lat, lon]).addTo(map);
+	marker.on('click', function(place)
+		{ 
+			url = "https://en.wikipedia.org/w/api.php?action=query&prop=info&format=json&inprop=url&titles="+place;
+			$.ajax({
+				url: url,
+			}).done(function (data) {
+				console.log(data);}); 
+
+	});
 });
 }
